d2ed6402f17e45f5754f441777f36c980b88417a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-05 22:37:59 +0600

    added all resource file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl font-bold'>
+        <h2>This is app jsx file</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
