5ce36e1bb3e85064d56e1714fb440ddd8b095799
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-06 02:15:00 +0600

    passing data

---
+++ src/components/Books/Book.jsx
@@ -0,0 +1,37 @@
+
+const Book = ({ book }) => {
+    const { image, bookName, author, category, review, tags, publisher, yearOfPublishing } = book;
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img className="h-[500px]"
+                    src={image}
+                    alt="book" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <h2 className="text-xl">{author}</h2>
+                <h3>{publisher}</h3>
+                <p>{yearOfPublishing}</p>
+                <div className="rating">
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="1 star" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="2 star" defaultChecked />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="3 star" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="4 star" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="5 star" />
+                </div>
+                <p className="mt-4">{review}</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">{category}</div>
+                    <br />
+                    <div className="badge badge-outline">{tags}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Book from "./Book";
 
 const Books = () => {
     const [books, setBooks] = useState([]);
@@ -8,10 +9,15 @@ const Books = () => {
             .then(data => setBooks(data))
     }, [])
     return (
-        <div>
-            <h2 className="text-4xl font-bold text-center">Books</h2>
-            <p>{books.length}</p>
-        </div>
+        <>
+            <h2 className="text-4xl font-bold text-center mt-5 pb-5 border-b-1">Books</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 m-6 gap-6">
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>
+        </>
+
     );
 };
 
