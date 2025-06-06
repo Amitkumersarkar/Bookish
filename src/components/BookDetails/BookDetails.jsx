9580acfcc96a50824e09a7201f64cc1be6404858
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-07 00:13:37 +0600

    added new routing file

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -2,12 +2,29 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
     const { bookId } = useParams();
+    const id = parseInt(bookId);
     const data = useLoaderData();
-    const book = data.find(book => book.bookId === bookId)
-    console.log(book);
+    const book = data.find(book => book.bookId === id)
+    console.log(book, data, bookId, id);
+
+    const { bookId: currentBookId, image, bookName, author, category, rating, publisher } = book;
     return (
-        <div>
-            <h2>book details : {bookId}</h2>
+        <div className="my-12">
+            <h2 className="text-xl mb-5 font-semibold">book details : {bookId}</h2>
+            <img className="w-48" src={image} alt="" />
+            <br />
+            <div>
+                <h2>{currentBookId}</h2>
+                <h2 className="font-bold">Book Name : {bookName}</h2>
+                <p className="font-semibold">Author : {author}</p>
+                <p>Publisher : {publisher}</p>
+                <p>Category : {category}</p>
+                <p>Rating : {rating}</p>
+
+            </div>
+            <br />
+            <button className="btn btn-ghost bg-cyan-700 text-white mb-7 mr-5">Read</button>
+            <button className="btn btn-ghost bg-fuchsia-800 text-white mb-7">WishList</button>
         </div>
     );
 };
